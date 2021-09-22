6a0c04471a578873c5c54b904f881d0b37cd30ac
unknown <alecxkendagor@gmail.com>
2021-09-22 11:42:45 +0300

    tesghs

--- app/Notifications/NewUserAlert.php
+++ app/Notifications/NewUserAlert.php
@@ -6,7 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
-
+use Mail;
 class NewUserAlert extends Notification
 {
     use Queueable;
